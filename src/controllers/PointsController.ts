22c20607652b84c0035b41f3976cc707d0b20787
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-06-03 11:37:39 -0300

    :sparkles: feat: Added index functionality

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -14,7 +14,7 @@ const PointsController = {
       items,
     } = request.body;
 
-    // const trx = await knex.transaction();
+    const trx = await knex.transaction();
 
     const point = {
       image: "image-fake",
@@ -27,7 +27,7 @@ const PointsController = {
       uf,
     };
 
-    const insertedIds = await knex("points").insert(point);
+    const insertedIds = await trx("points").insert(point);
 
     const point_id = insertedIds[0];
 
@@ -38,13 +38,27 @@ const PointsController = {
       };
     });
 
-    await knex("point_items").insert(pointItems);
+    await trx("point_items").insert(pointItems);
 
-    return response.json({ sucess: true });
+    await trx.commit();
+
+    return response.json({ id: point_id, ...point });
   },
 
   async index(request: Request, response: Response) {
-    const points = await knex("points").select("*");
+    const { city, uf, items } = request.query;
+
+    const parsedItems = String(items)
+      .split(",")
+      .map((item) => Number(item.trim()));
+
+    const points = await knex("points")
+      .join("point_items", "points.id", "=", "point_items.point_id")
+      .whereIn("point_items.item_id", parsedItems)
+      .where("city", String(city))
+      .where("uf", String(uf))
+      .distinct()
+      .select("points.*");
 
     return response.json(points);
   },
@@ -60,7 +74,8 @@ const PointsController = {
 
     const items = await knex("items")
       .join("point_items", "items.id", "=", "point_items.item_id")
-      .where("point_items.point_id", id);
+      .where("point_items.point_id", id)
+      .select("items.title");
 
     return response.json({ point, items });
   },
