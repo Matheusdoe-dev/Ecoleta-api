d816db00af6f12c9098d08fc4285df5b57a9b21f
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-06-03 12:44:44 -0300

    :construction: chore: Added Procfile

--- src/server/server.ts
+++ src/server/server.ts
@@ -5,6 +5,8 @@ import routes from "../routes";
 
 const app = express();
 
+const port = process.env.PORT || 3333;
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
@@ -14,4 +16,4 @@ app.use(
   express.static(path.resolve(__dirname, "..", "..", "uploads"))
 );
 
-app.listen(3333);
+app.listen(port);
