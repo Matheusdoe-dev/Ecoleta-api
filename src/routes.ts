5e15216b0bafb4f6cc18da39351862974f81dbf9
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-06-06 17:50:48 -0300

    :construction: feat: Some changes
    Validating routes.post('/points')... with celebrate/Joi, using multer to upload single image in routes.post('/points')...

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,42 @@
 import express from "express";
+import { celebrate, Joi } from "celebrate";
+
+import multer from "multer";
+import multerConfig from "./config/multer";
 
 // Controllers
 import ItemsController from "./controllers/ItemsController";
 import PointsController from "./controllers/PointsController";
 
 const routes = express.Router();
+const upload = multer(multerConfig);
 
 // listagem de items
 routes.get("/items", ItemsController.index);
 
 // criação de pontos de coleta
-routes.post("/points", PointsController.create);
+routes.post(
+  "/points",
+  upload.single("image"),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required,
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    }
+  ),
+  PointsController.create
+);
 
 // listagem de pontos de coleta
 routes.get("/points", PointsController.index);
